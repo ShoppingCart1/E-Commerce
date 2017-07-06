5e1ac15519fb78ea21026b6418dbdbe11100e093
Shruti9520 <shrutisingh7219@gmail.com>
2017-07-06 16:55:40 +0530

    controllers

--- E-Commerce/src/main/webapp/module/home/HomeCtrl.js
+++ E-Commerce/src/main/webapp/module/home/HomeCtrl.js
@@ -1,6 +1,6 @@
 var app= angular.module('mivimHome',[]);
  
-   app.controller("HomeCtrl", function($scope) {
+   app.controller("HomeCtrl", function($scope, $http) {
 	   $http.get("item.json").then(function(response) {
 			$scope.myData = response.data.records;
 		});	  
--- E-Commerce/src/main/webapp/module/login/LoginCtrl.js
+++ E-Commerce/src/main/webapp/module/login/LoginCtrl.js
@@ -1,5 +1,23 @@
  var app= angular.module('mivimNonSecured',[]);
  
-   app.controller("LoginCtrl", function($scope) {
-		  
+   app.controller("LoginCtrl", function($scope,$http) {
+	   $scope.sendPost = function() {
+	        $http({
+	            url : 'authentication',
+	            method : "POST",
+	            data : {
+	                'email' : $scope.email,
+	                'password' : $scope.password
+	            }
+	        }).then(function(response) {
+	            console.log(response.data);
+	            $scope.message = response.data;
+	        }, function(response) {
+	            //fail case
+	            console.log(response);
+	            $scope.message = response;
+	        });
+	 
+	    };
+	   
    });
